d75fa49812544eb9178097cf288021373bc84b7f
miracle <1772176802@qq.com>
2019-07-16 23:16:52 +0800

    修改my模块

--- lib/pages/my.dart
+++ lib/pages/my.dart
@@ -5,11 +5,26 @@ class My extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('一点生鲜'),
-      ),
-      body: Text('我的'),
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 120.0,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Color(0xFF58C7FA), Color(0xFF52BBF9), Color(0xFF4CAFF8)]),
+            image: DecorationImage( image: NetworkImage('http://puqovkxfe.bkt.clouddn.com/image/mine/mine_user_background.png')),
+          ),
+        ),
+        Center(
+          child: Container(
+            width: 720.0,
+            height: 120.0,
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0xFFFFFF00), width: 0.5)
+            ),
+          ),
+        ),
+        Row(children: <Widget>[],)
+      ],
     );
   }
 }
