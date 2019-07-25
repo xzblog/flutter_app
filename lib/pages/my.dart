50c2045e01d0a0b9208b822668689075508035d6
zhangshengfan <1772176802@qq.com>
2019-07-25 18:28:34 +0800

    my页面修改

--- lib/pages/my.dart
+++ lib/pages/my.dart
@@ -5,35 +5,78 @@ class My extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          height: 120.0,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [Color(0xFF58C7FA), Color(0xFF52BBF9), Color(0xFF4CAFF8)]),
-            image: DecorationImage( 
-              image: NetworkImage('http://puqovkxfe.bkt.clouddn.com/image/mine/mine_user_background.png'), 
-              fit: BoxFit.cover
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(96),
+        child: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: (){
+              print('设置');
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.face),
+              onPressed: (){
+                print('客服');
+              },
+            )
+          ],
+          backgroundColor: Colors.transparent,
+          flexibleSpace: Container(
+            height: 120.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Color(0xFF58C7FA), Color(0xFF52BBF9), Color(0xFF4CAFF8)]),
+              image: DecorationImage( 
+                image: NetworkImage('http://puqovkxfe.bkt.clouddn.com/image/mine/mine_user_background.png'), 
+                fit: BoxFit.cover
+              ),
             ),
           ),
         ),
-        Row(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Container(
-                width: 340.0,
-                height: 120.0,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Color(0xFF000000), width: 1)
-                ),
-              )
+      ),
+      body: ListView(
+        children: <Widget>[
+          
+          Container(
+            height: 120.0,
+            margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0xFF000000), width: 1),
+              borderRadius: BorderRadius.circular(6.0),
+              color: Color(0XFFFFFFFF),
             ),
-          ],
-        ),
-        Row(children: <Widget>[],)
-      ],
+          ),
+          Container(
+            height: 120.0,
+            margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0xFF000000), width: 1),
+              borderRadius: BorderRadius.circular(6.0),
+              color: Color(0XFFFFFFFF),
+            ),
+          ),
+          Container(
+            height: 120.0,
+            margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0xFF000000), width: 1),
+              borderRadius: BorderRadius.circular(6.0),
+              color: Color(0XFFFFFFFF),
+            ),
+          ),
+          Container(
+            height: 120.0,
+            margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0xFF000000), width: 1),
+              borderRadius: BorderRadius.circular(6.0),
+              color: Color(0XFFFFFFFF),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
